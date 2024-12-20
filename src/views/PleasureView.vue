05691a51d5ffd1bb1dbbe1e0a193b0d7f8abe67c
Kamilla289 <kami.rivay56@gmail.com>
2024-12-20 16:25:42 +0500

    fix: image links NoCor

--- src/views/PleasureView.vue
+++ src/views/PleasureView.vue
@@ -14,11 +14,11 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-4 offset-2">
-            <img class="shop__girl" :src="`img/coffee_goods.jpg`" alt="girl" />
+            <img class="shop__girl" src="`img/coffee_goods.jpg`" alt="girl" />
           </div>
           <div class="col-lg-4">
             <div class="title">About our beans</div>
-            <img class="beanslogo" :src="`logo/Beans_logo_dark.svg`" alt="Beans logo" />
+            <img class="beanslogo" src="`logo/Beans_logo_dark.svg`" alt="Beans logo" />
             <div class="shop__text">
               Extremity sweetness difficult behaviour he of. On disposal of as
               landlord horrible.
--- src/views/ThanksView.vue
+++ src/views/ThanksView.vue
@@ -15,7 +15,7 @@
         <div class="row">
           <div class="col col-12 col-lg-6 offset-0 offset-lg-3">
             <div class="title mt-5">Tell us about your tastes</div>
-            <img class="beanslogo mt-5" :src="`logo/Beans_logo_dark.svg`" alt="Beans logo" />
+            <img class="beanslogo mt-5" src="`logo/Beans_logo_dark.svg`" alt="Beans logo" />
           </div>
         </div>
 
@@ -24,7 +24,7 @@
             <div class="title big__text mt-5">
               <p>Thank you so much</p>
               <p>We contact you as soon as posible</p>
-              <img :src="`img/thanks.png`" alt="thanks-icon" />
+              <img src="`img/thanks.png`" alt="thanks-icon" />
             </div>
           </div>
         </div>
